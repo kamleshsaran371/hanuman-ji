31dad051fd99ac104cc30a2fd2d10a90d079cc11
kamleshsaran371 <kamleshsaran371@gmail.com>
2025-11-29 16:13:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "28446111"
+api_hash = "4ef8f43ed7d3f22b1e3acc40e86d7506"
+bot_token = "8591382413:AAFW_e36UbfLMH1i1sEV9ghYJ2KvfjnzJGs"
+auth_users = [1956027694]
+sudo_users = []
