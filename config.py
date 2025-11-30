55382faeb7b2e0b2e921bbe2faa90e156bc5d7c6
kamleshsaran371 <kamleshsaran371@gmail.com>
2025-11-30 22:57:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "28446111"
 api_hash = "4ef8f43ed7d3f22b1e3acc40e86d7506"
-bot_token = "8597546466:AAG56NGJYl1YRNtEVDRgXl9Cs1NWQX0h1-s"
+bot_token = "8281504450:AAH1q7AJRRlpaHKOMx4X0AEvU9ax0_N_-Ew"
 auth_users = [1956027694]
 sudo_users = []
